b3305170b07f3f86f44dfa1a6846e21272bd6f31
Cheng Gong <cheng.gong@mail.mcgill.ca>
2013-11-16 16:02:52 -0500

    fixed some minor error

--- src/GUI/CrAccPanel.java
+++ src/GUI/CrAccPanel.java
@@ -12,35 +12,35 @@ import javax.swing.*;
 public class CrAccPanel {
 	private JPanel mainPanel = new JPanel();
 	private JLabel title = new JLabel("Create Account");
-	
-    protected static final String usernameField = "username";
-    protected static final String passwordField = "password";
-    
-    public final int WIDTH = 900;
-    public final int HEIGHT = WIDTH * 9 / 16;
-    public final int SCALE = 1;
 
-    protected JPasswordField password = new JPasswordField(10);
-    protected JTextField username = new JTextField(10);
-    protected JLabel actionLabel;
-    private JButton crAcc;
-    private JButton back;
-    private JLabel textFieldLabel;
-    private JLabel passwordFieldLabel;
-	
-    public CrAccPanel() {
+	protected static final String usernameField = "username";
+	protected static final String passwordField = "password";
 
-        mainPanel.setLayout(null);
-        
-        crAcc = new JButton("Create New Account");
-        back = new JButton("Back");
-        
-        textFieldLabel = new JLabel(usernameField + ": ");
-        textFieldLabel.setLabelFor(username);
-        passwordFieldLabel = new JLabel(passwordField + ": ");
-        passwordFieldLabel.setLabelFor(password);
-        actionLabel = new JLabel("");
-        
+	public final int WIDTH = 900;
+	public final int HEIGHT = WIDTH * 9 / 16;
+	public final int SCALE = 1;
+
+	protected JPasswordField password = new JPasswordField(10);
+	protected JTextField username = new JTextField(10);
+	protected JLabel actionLabel;
+	private JButton crAcc;
+	private JButton back;
+	private JLabel textFieldLabel;
+	private JLabel passwordFieldLabel;
+
+	public CrAccPanel() {
+
+		mainPanel.setLayout(null);
+
+		crAcc = new JButton("Create New Account");
+		back = new JButton("Back");
+
+		textFieldLabel = new JLabel(usernameField + ": ");
+		textFieldLabel.setLabelFor(username);
+		passwordFieldLabel = new JLabel(passwordField + ": ");
+		passwordFieldLabel.setLabelFor(password);
+		actionLabel = new JLabel("");
+
 		mainPanel.add(title);
 		mainPanel.add(crAcc);
 		mainPanel.add(back);
@@ -50,7 +50,7 @@ public class CrAccPanel {
 		mainPanel.add(username);
 		mainPanel.add(password);
 
-		Dimension size = new Dimension(100,25);
+		Dimension size = new Dimension(100, 25);
 		title.setBounds(400, 50, size.width, size.height);
 		crAcc.setBounds(350, 350, 200, size.height);
 		back.setBounds(400, 450, size.width, size.height);
@@ -58,53 +58,44 @@ public class CrAccPanel {
 		passwordFieldLabel.setBounds(350, 200, size.width, size.height);
 		username.setBounds(450, 150, size.width, size.height);
 		password.setBounds(450, 200, size.width, size.height);
-<<<<<<< HEAD
-		actionLabel.setBounds(350, 220, 500, size.height+100);
-=======
-		actionLabel.setBounds(350, 250, 200, size.height);
->>>>>>> ca1294b4af86390388cd4021c252b885efc85b03
-		
-		crAcc.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-<<<<<<< HEAD
+		actionLabel.setBounds(350, 220, 500, size.height + 100);
 
-					String enteredUsername = username.getText();
-					String enteredPassword = new String(password.getPassword());
-					
-					boolean success = CreateAccount.addUser(enteredUsername, enteredPassword);
-					
-					if(!success){
-						String errorMessage = "";
-						try {
-							if(CSVHandler.isExist(enteredUsername)) errorMessage = errorMessage + "Username Already Exists.<br>";
-							errorMessage = errorMessage + CSVHandler.isValidUsername(enteredUsername);
-							errorMessage = errorMessage + CSVHandler.isValidPassword(enteredPassword);
-							actionLabel.setText("<html>"+ errorMessage+"</html>");
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
+
+
+		crAcc.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				String enteredUsername = username.getText();
+				String enteredPassword = new String(password.getPassword());
+
+				boolean success = CreateAccount.addUser(enteredUsername,
+						enteredPassword);
+
+				if (!success) {
+					String errorMessage = "";
+					try {
+						if (CSVHandler.isExist(enteredUsername))
+							errorMessage = errorMessage
+									+ "Username Already Exists.<br>";
+						errorMessage = errorMessage
+								+ CSVHandler.isValidUsername(enteredUsername);
+						errorMessage = errorMessage
+								+ CSVHandler.isValidPassword(enteredPassword);
+						actionLabel.setText("<html>" + errorMessage + "</html>");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
 					}
-					else{
-						actionLabel.setText("Your Account Has Been Created.");
-=======
-					boolean success = true;
-					
-					String enteredUsername = username.getText();
-					
-					if(!success){
-						actionLabel.setText("Unsuccessful Account Creation.");
-					}
-					else{
-						actionLabel.setText(enteredUsername);
->>>>>>> ca1294b4af86390388cd4021c252b885efc85b03
-					}
+				} else {
+					actionLabel.setText("Your Account Has Been Created.");
+
 				}
+			}
 		});
-  
-    }
-	
+
+	}
+
 	public void addBackBtnActionListener(ActionListener listener) {
 		back.addActionListener(listener);
 	}
