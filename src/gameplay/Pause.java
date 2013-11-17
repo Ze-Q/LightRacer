3dbe349bc38924987f7b0e9cbf372f71cc766a12
Dmitry M <dmurzich@gmail.com>
2013-11-17 17:44:31 -0500

    Pause.java code maintenance and made it resume the game on pressing X

--- src/gameplay/Pause.java
+++ src/gameplay/Pause.java
@@ -1,32 +1,45 @@
 package gameplay;
 
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+
 
 public class Pause {
 		
-	public static final int WIDTH = 300;
-	public static final int HEIGHT = WIDTH * 9 / 16 ;
-	JFrame frame = new JFrame();
-	boolean resume;
-	Game game;
+	private JFrame frame = new JFrame();
+	private boolean resume;
+	private Game game;
 	
 	public Pause(Game game) {
 		this.game = game;
 		resume = false;
-		JButton button = new JButton("Resume");
-		frame.add(button);
-		frame.setLayout(new FlowLayout());
-		frame.setSize(200,200);
+		JButton button = new JButton("Press to Resume");
+		button.setSize(100,100);
+		button.setFont(new Font("Dialog", 1, 15));
+		frame.setLayout(new BorderLayout());
+		frame.add(button, BorderLayout.CENTER);
+		frame.setSize(200,100);
 		frame.setResizable(false);
-    frame.setTitle("Paused");
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				resume();
+				frame.setVisible(false);
+				frame.dispose();
+			}
+		});
+		frame.setTitle("Paused");
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 		button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -37,11 +50,9 @@ public class Pause {
 		});
 	}
 	
-	public void resume() {
+	private void resume() {
 		game.resume = true;
 		game.key.pause = false;
 	}
-	
-	
 	
 }
