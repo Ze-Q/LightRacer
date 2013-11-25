e9ca5bd0df1b7fe20aa894108dbd406a8eef9767
Ze Qian Zhang <ze.q.zhang@mail.mcgill.ca>
2013-11-24 22:31:31 -0500

    Added javadoc comments to StatsFileSystem. Also changed the path of the stats.csv file.

--- src/crud/StatsFileSystem.java
+++ src/crud/StatsFileSystem.java
@@ -7,59 +7,55 @@ import java.io.*;
  *
  */
 public class StatsFileSystem {
-	
-	/**
-	 * Singleton design, the only instance of this StatsFileSystem object
-	 */
+
 	private static final StatsFileSystem INSTANCE = new StatsFileSystem();
 	
-	/**
-	 * The maximum number of users is defined to be 50 in this case, but it can be changed easily if we need to have more users.
-	 */
-	private int maxNumOfUsers = 50;
+	private int maxNumOfUsers = 100;
 	
-	/**
-	 * This private array stores all the stats information in the memory
-	 */
 	private String statsArray[][];
 	
-	/**
-	 * Private constructor for the singleton design, Read from the stats file on the disk
-	 */
 	private StatsFileSystem() {
 		readStatsFromFile();
 	}
 	
 	/**
-	 * The getter for singleton design, make sure that there is only one instance of this object.
-	 * @return return the only instance of the object
+	 * The getter for singleton design, it makes sure that there is only one instance of this object.
+	 * @return return the only instance of the StatsFileSystem object
 	 */
 	public static StatsFileSystem getInstance() { 
         return INSTANCE;
     }
 	
-	/**
-	 * This method generates a new stats array with a '0' at [0][0]
-	 * @param size take in a variable size which determines the size of the stats array
-	 */
-	public void generateNewStatsArray(int size) {
+
+	private void generateNewStatsArray(int size) {
 		statsArray = new String[size][size];
 		statsArray[0][0]="0";
 	}
 	
 	/**
-	 * Return the stats array
-	 * @return return the stats array object
+	 * Return the stats array contained in the StatsFileSystem object
+	 * @return return the stats array, which contains the versus records of all players
 	 */
 	public String[][] getStatsArray() {
 		return statsArray;
 	}
 	
+	/**
+	 * This method sets a new maximum number of users. 
+	 * <p>
+	 * It means that the stats array will have this number as the new size.
+	 * This method is currently not used, it is here to allow the future developers to 
+	 * increase the amount of users this program can hold.
+	 * The default maximum number of users is currently 100.
+	 * @param newMaxNumOfUsers Takes in an int to set the new maximum of users.
+	 */
 	public void setMaxNumOfUsers(int newMaxNumOfUsers) {
 		maxNumOfUsers=newMaxNumOfUsers;
 	}
 	
-	//print the array to stdout, for testing purpose only
+	/**
+	 * print the array to stdout, for testing purpose only
+	 */
 	public void printStatsArray() {
 		for(int row=0; row<maxNumOfUsers; row++){
 			for(int column=0; column<maxNumOfUsers; column++){
@@ -74,10 +70,14 @@ public class StatsFileSystem {
 		}
 	}
 	
+	/**
+	 * This method is used to read the stats data from file.
+	 * @return return true if success, false if fail.
+	 */
 	public boolean readStatsFromFile() {
 		try{
 			generateNewStatsArray(maxNumOfUsers);
-			File dir = new File ("./res");
+			File dir = new File ("./res/data");
 			File statsCSV = new File(dir, "stats.csv");
 		
 			//check whether userCSV already exists, if not then do not read anything
@@ -112,6 +112,10 @@ public class StatsFileSystem {
 		}
 	}
 	
+	/**
+	 * Write the information currently contained in the stats array to the file on hard drive. 
+	 * @return return true if success, false if fail.
+	 */
 	public boolean writeStatsToFile() {
 		try{
 			
@@ -158,6 +162,12 @@ public class StatsFileSystem {
 		
 	}
 	
+	/**
+	 * This method looks for the number of times that user1 won against user2.
+	 * @param user1 A string which contains the username of user1.
+	 * @param user2 A string which contains the username of user2.
+	 * @return return the number of wins as a string
+	 */
 	public String searchForVersus (String user1, String user2) {
 		int rowPosition = 0;
 		int columnPosition = 0;
@@ -178,6 +188,13 @@ public class StatsFileSystem {
 		return statsArray[rowPosition][columnPosition];
 	}
 	
+	/**
+	 * This method updates the versus records of two users user1 and user2 in the stats array.
+	 * @param user1 a String that is the username of user1.
+	 * @param user1Wins the number of times that user1 won against user2.
+	 * @param user2 a String that is the username of user2.
+	 * @param user2Wins the number of times that user2 won against user1.
+	 */
 	public void updateRecords (String user1, int user1Wins, String user2, int user2Wins) {
 		int rowPosition;
 		int columnPosition;
@@ -216,6 +233,11 @@ public class StatsFileSystem {
 		return columnPosition;
 	}
 	
+	/**
+	 * Add a new user to the stats array, with the corresponding 0s 
+	 * since this player has not played any game yet.
+	 * @param username this method takes in a string that is the username of the new user.
+	 */
 	public void addNewUserToArray (String username) {
 		int rowPosition=0;
 		for(int row = 1; row<statsArray.length; row++) {
